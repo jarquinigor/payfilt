b254c082221b9dbfe2e1542fb4a0d3bf88e481c6
Javier Junior Arquinigo Rivas <jarquinigorupc@gmail.com>
2023-11-05 11:02:00 -0500

    flask mail

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = config('MAIL_USERNAME')
     MAIL_PASSWORD = config('MAIL_PASSWORD')
 
